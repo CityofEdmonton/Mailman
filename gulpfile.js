533d70d51f6614a88c2dcc50fbf46397bd733883
Jared Rewerts <jared.rewerts@edmonton.ca>
2016-09-09 14:44:40 -0600

    Created a basic gulp task for browserify. Added browserify to npm dev dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
 
-gulp.task('default', function() {
+gulp.task('default', function () {
   // place code for your default task here
+});
+
+gulp.task('browserify', function () {
+  return browserify('./src/client/client.js')
+    .bundle()
+    .on('error', function () {
+      gutil.log(e);
+    })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./build/production'));
 });
