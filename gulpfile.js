db4198eb2007cc7a75c588ff0acfe7a3b006e95e
Jared Rewerts <jared.rewerts@edmonton.ca>
2016-09-12 08:28:11 -0600

    Formatted our Gulp file. Closes #6.

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,18 @@ var gutil = require('gulp-util');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 
+/**
+ * Runs the default build tasks.
+ *
+ */
 gulp.task('default', function () {
   // place code for your default task here
 });
 
+/**
+ * Bundles up client.js (and all required functionality) and places it in a build directory.
+ *
+ */
 gulp.task('browserify', function () {
   return browserify('./src/client/client.js')
     .bundle()
