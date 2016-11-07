947860592a53bfd835cc20383c8c67fb0369a4ce
Jared Rewerts <jared.rewerts@edmonton.ca>
2016-11-07 15:55:01 -0700

    Resolves issues with the layout of the RTE. Closes #41.

--- src/client/js/rich-text-editor.js
+++ src/client/js/rich-text-editor.js
@@ -4,7 +4,8 @@ require('./wysiwyg-editor.js');
 //var Intercom = require('./intercom.js');
 
 $(document).ready(function() {
-  $('#editor').wysiwyg();
+  var wysiwyg = $('#editor').wysiwyg();
+  var selection;
 
   /**
    * The ID of this dialog. This is set once when the template is rendered.
@@ -24,6 +25,10 @@ $(document).ready(function() {
   console.log('Dialog: ' + DIALOG_ID);
 
   var button = $('#done').on('click', onSubmit);
+  var linkButton = $('#link-btn').on('click', onLinkClick);
+  var linkInsertButton = $('#link-insert-btn').on('click', onLinkInsertClick);
+  var linkInput = $('#link-url-value').on('click', wysiwyg.saveSelection)
+
 
   // Sets up an interval to check-in with the server every few seconds, so we can tell
   // if it's been X-ed out.
@@ -31,17 +36,38 @@ $(document).ready(function() {
     intercom.emit(DIALOG_ID, {state: 'checkIn'});
   }, CHECKIN_INTERVAL_MS);
 
-  /**
-   * Runs when the form is submitted.
-   */
-  function onSubmit() {
-    // Here is where you would add custom logic specific to your form.
-    // You may need to make additional google.script.run calls to store various information
-    // collected in the dialog.
+  function onLinkInsertClick(e) {
+    var urlInput = $('#link-url-value');
 
+    wysiwyg.restoreSpecificSelection(selection);
 
-	   console.log('RTE: Submitting... ' + $('#editor').cleanHtml());
-    intercom.emit(DIALOG_ID, {state: 'done', message: $('#editor').cleanHtml()});
-    google.script.host.close();
+    console.log(urlInput.val());
+    wysiwyg.execCommand('createLink', urlInput.val());
+
+    var menu = $('#rte-link-menu');
+    menu.addClass('hidden');
+
   }
+
+  function onLinkClick(e) {
+    selection = wysiwyg.getCurrentRange();
+
+    var menu = $('#rte-link-menu');
+    menu.toggleClass('hidden');
+  }
+
 });
+
+/**
+ * Runs when the form is submitted.
+ */
+function onSubmit() {
+  // Here is where you would add custom logic specific to your form.
+  // You may need to make additional google.script.run calls to store various information
+  // collected in the dialog.
+
+
+  console.log('RTE: Submitting... ' + $('#editor').cleanHtml());
+  intercom.emit(DIALOG_ID, {state: 'done', message: $('#editor').cleanHtml()});
+  google.script.host.close();
+}
