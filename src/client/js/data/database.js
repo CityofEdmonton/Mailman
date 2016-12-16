1f2b5c8a539c940a50c61c62d430ae8a1529809f
Jared Rewerts <jared.rewerts@edmonton.ca>
2016-12-16 10:54:46 -0700

    Fixed the sheet id problem. Closes #73.

--- src/client/js/data/database.js
+++ src/client/js/data/database.js
@@ -9,11 +9,17 @@ var Database = function() {
   /**
    * Converts the returned String into an Object. If the string isn't valid JSON, an error is thrown.
    *
-   * @param {String} jsonString The stringified data value to be converted into an object.
-   * @param {Function} callback The function to call after parsing is successful.
+   * @param {string} jsonString The stringified data value to be converted into an object.
+   * @param {Object} callback The object that stores the success and failure handlers.
+   * @param {Function} callback.success The function to call after parsing is successful.
+   * @param {Function} callback.failure The function to call after parsing fails.
    */
   var handleJSONParsing = function(jsonString, callback) {
     if (jsonString === undefined) {
+      if (callback.failure) {
+        callback.failure();
+      }
+
       return;
     }
 
@@ -24,10 +30,15 @@ var Database = function() {
     }
     catch (e) {
       console.log('Failed when parsing JSON: ' + jsonString);
-      throw e;
+      if (callback.failure) {
+        callback.failure();
+      }
+      else {
+        throw e;
+      }
     }
 
-    callback(obj);
+    callback.success(obj);
   };
 
   // ***** privileged methods ***** //
@@ -58,11 +69,25 @@ var Database = function() {
    * Loads an object.
    *
    * @param {String} key The key of the Object to return.
-   * @param {Function} callback The function to call on success
+   * @param {Function} callback The function to call on success.
+   * @param {Function} failure The function to call on failure.
    */
-  this.load = function(key, callback) {
+  this.load = function(key, callback, failure) {
+    var cbObject;
+    if (failure !== undefined) {
+      cbObject = {
+        success: callback,
+        failure: failure
+      };
+    }
+    else {
+      cbObject = {
+        success: callback
+      };
+    }
+
     google.script.run
-        .withUserObject(callback)
+        .withUserObject(cbObject)
         .withSuccessHandler(handleJSONParsing)
         .load(key);
   };
