f517be5a2296d27200ae3438cd58e6f6f3934b37
Jared Rewerts <jared.rewerts@edmonton.ca>
2016-12-28 13:28:40 -0700

    Converted TextareaCard to use the new autocomplete system. Closes #36.

--- src/client/js/card/card-textarea.js
+++ src/client/js/card/card-textarea.js
@@ -1,83 +1,41 @@
-var TitledCard = require('./card-titled.js');
 var textareaHTML = require('./card-textarea.html');
+var TitledCard = require('./card-titled.js');
+var AutocompleteConfig = require('./autocomplete-config.js');
 
 
 
+/**
+ * TextareaCards use a multiline input field. They differ from InputCard in this way.
+ *
+ * @param {jquery} appendTo The object to append this Card to.
+ * @param {Object} options The configuration options for this TextareaCard.
+ * @param {String} options.label The label the textarea should have when it has no text.
+ * @param {Object} options.autocomplete The autocomplete configuration object. Please see setAutocomplete for a more
+ * detailed listing of this Object.
+ * @constructor
+ */
 var TextareaCard = function(appendTo, options) {
   TitledCard.call(this, appendTo, options);
 
   // Private variables
   var self = this;
   var innerBase = $(textareaHTML);
-
-  // Public Variables
-
-  //***** Private Methods *****//
-  /**
-   * Gets the jQuery UI autocomplete config.
-   *
-   * @param {string} append The string to put after a selection is made.
-   * @param {string} prepend The string to put before the selection.
-   * @param {number} maxResults The maximum number of results displayed when filtering results.
-   * @param {string} trigger A string to watch for that triggers selection.
-   * @param {Array<string>} results The results to filter for autocomplete.
-   * @return {object} A configuration object used by jQuery UI.
-   */
-  var getAutocompleteConfig = function(append, prepend, maxResults, trigger, results) {
-    return {
-      minLength: 0,
-      source: function(request, response) {
-        if (trigger === undefined) {
-          response($.ui.autocomplete.filter(results, request.term.split(/,\s*/).pop()).slice(0, maxResults));
-        }
-        else {
-          var last = request.term.split(trigger).pop();
-
-          // Fixes weird bug that doesn't force the DDL to hide if you trigger it with nothing.
-          if (trigger !== '' && request.term === '') {
-            response('');
-          }
-          else {
-            // delegate back to autocomplete, but extract the last term
-            response($.ui.autocomplete.filter(results, last).slice(0, maxResults));
-          }
-        }
-      },
-      focus: function() {
-        return false;
-      },
-      select: function(event, ui) {
-        if (trigger === undefined) {
-          var terms = this.value.split(/,\s*/);
-          terms.pop();
-          terms.push(prepend);
-          terms.push(ui.item.value);
-          terms.push(append);
-          this.value = terms.join('');
-        }
-        else {
-          var terms = [this.value.substring(0, this.value.lastIndexOf(trigger))];
-
-          terms.push(prepend);
-          terms.push(ui.item.value);
-          terms.push(append);
-          this.value = terms.join('');
-        }
-
-        // We have to manually mark the text field as dirty. If we don't, MDL text fields act weird.
-        $(this).parent().addClass('is-dirty');
-
-        return false;
-      }
-    };
-  };
-
+  var textarea = innerBase.find('textarea');
+  var acConfig = new AutocompleteConfig(textarea);
 
   //***** Privileged Methods *****//
+
   /**
-   * Sets autocomplete bsaed upon some options.
+   * Sets autocomplete based upon some options.
    *
-   * @param {object} options The options to set up autocomplete.
+   * @param {Object} options The options to set up autocomplete.
+   * @param {String} options.trigger A String that causes autocomplete to drop down.
+   * @param {String} options.append A String to append after a selection is made.
+   * @param {String} options.prepend A String to prepend to your selection.
+   * @param {Number} options.maxResults A Number indicating the maximum number of displayed results.
+   * @param {Boolean} options.triggerOnFocus A value indicating whether the autocomplete should trigger when focused.
+   * This brings it more in line with the behaviour of a drop down list.
+   * @param {Array<String>} options.results The list of results to filter through.
    */
   this.setAutocomplete = function(options) {
     var append = '';
@@ -85,7 +43,6 @@ var TextareaCard = function(appendTo, options) {
     var maxResults;
     var trigger;
     var results = [];
-    var input = innerBase.find('textarea');
 
     if (options.trigger !== undefined) {
       trigger = options.trigger;
@@ -100,38 +57,38 @@ var TextareaCard = function(appendTo, options) {
       maxResults = options.maxResults;
     }
     if (options.triggerOnFocus === true) {
-      input.on('focus', function() {input.autocomplete('search', self.getValue())});
+      textarea.on('focus', function() {textarea.autocomplete('search', self.getValue())});
     }
     if (options.results !== undefined) {
       results = options.results;
     }
 
-    input.autocomplete(getAutocompleteConfig(append, prepend, maxResults, trigger, results));
+    textarea.autocomplete(acConfig.getAutocompleteConfig(append, prepend, maxResults, trigger, results));
   };
 
   /**
-   * Gets the value in the input.
+   * Gets the value in the textarea.
    *
-   * @return {string} The value in the input.
+   * @return {String} The value in the textarea.
    */
   this.getValue = function() {
-    return innerBase.find('textarea').val();
+    return textarea.val();
   };
 
   /**
-   * Sets the value of the input.
+   * Sets the value of the textarea.
    *
-   * @param {string} value The value to set in the input.
+   * @param {String} value The value to set in the textarea.
    */
   this.setValue = function(value) {
-    innerBase.find('textarea').val(value);
+    textarea.val(value);
     innerBase.addClass('is-dirty');
   };
 
   /**
-   * Sets the label shown in the input when nothing has been typed.
+   * Sets the label shown in the textarea when nothing has been typed.
    *
-   * @param {string} label The value to set as the label.
+   * @param {String} label The value to set as the label.
    */
   this.setLabel = function(label) {
     innerBase.find('label').text(label);
@@ -156,8 +113,6 @@ var TextareaCard = function(appendTo, options) {
 /** */
 TextareaCard.prototype.constructor = TextareaCard;
 TextareaCard.prototype = Object.create(TitledCard.prototype);
-
-//***** Public Methods *****//
 
 
 /** */
