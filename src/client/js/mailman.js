592a8cc438e48412528362cf3dfb7a926cadaa79
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-01-03 12:25:38 -0700

    Cleaned up Mailman. Removed references to intercom. These can be added again later on if needed.

--- src/client/js/mailman.js
+++ src/client/js/mailman.js
@@ -1,11 +1,4 @@
-/**
- * Intercom: https://github.com/diy/intercom.js/
- * Tips on using intercom with GAS: https://github.com/googlesamples/apps-script-dialog2sidebar
- *
- *
- */
 
-var Util = require('./util.js');
 var PubSub = require('pubsub-js');
 var Rules = require('./data/rule-container.js');
 var RuleTypes = require('./data/rule-types.js');
@@ -14,7 +7,8 @@ var Database = require('./data/database.js');
 var Keys = require('./data/prop-keys.js');
 var RulesListView = require('./rules/rules-list-view.js');
 var CardsView = require('./cards/cards-view.js');
-//var Intercom = require('./intercom.js');
+
+
 
 var MailMan = function() {
 
@@ -24,20 +18,7 @@ var MailMan = function() {
 
   var self;
 
-  // The object used to communicate between the sidebar and the RTE (Rich Text Editor)
-  var intercom;
-
-	// How long to wait for the dialog to check-in before assuming it's been closed, in milliseconds.
-	var DIALOG_TIMEOUT_MS = 2000;
-
   var database = new Database();
-
-	/**
-	 * Holds a mapping from dialog ID to the ID of the timeout that is used to
-	 * check if it was lost. This is needed so we can cancel the timeout when
-	 * the dialog is closed.
-	 */
-	var timeoutIds = {};
 
   var rules;
 
@@ -54,11 +35,8 @@ var MailMan = function() {
   this.init = function() {
     self = this;
 
-    // UI Configuration
-    // All UI Bindings
     $('#help').on('click', self.onHelpClick);
 
-    intercom = Intercom.getInstance();
     rulesListView = new RulesListView($('#layout-container'));
     cardsView = new CardsView($('#layout-container'));
 
@@ -135,72 +113,6 @@ var MailMan = function() {
   };
 
   //***** PRIVATE *****//
-
-  /**
-   * Using the Cards handler, this sets the state of the buttons.
-   * Depending on the active Card, different buttons may be shown.
-   *
-   */
-  var setButtonState = function() {
-    // Default state
-    Util.setHidden($('#done'), true);
-    Util.setHidden($('#step'), false);
-    Util.setHidden($('#back'), false);
-
-    if (cards.isFirst()) {
-      Util.setHidden($('#back'), true);
-    }
-    else if (cards.isLast()) {
-      Util.setHidden($('#done'), false);
-      Util.setHidden($('#step'), true);
-    }
-  };
-
-  var onRTEOpened = function(dialogId) {
-
-    intercom.on(dialogId, function(data) {
-
-          switch (data.state) {
-            case 'done':
-              console.log('Dialog submitted.\n');
-
-              getNode('Body').data.setValue(data.message);
-
-              forget(dialogId);
-              break;
-            case 'checkIn':
-              forget(dialogId);
-              watch(dialogId);
-              break;
-            case 'lost':
-              console.log('Dialog lost.\n');
-              break;
-            default:
-              throw 'Unknown dialog state: ' + data.state;
-          }
-        });
-  };
-
-  /**
-   * Watch the given dialog, to detect when it's been X-ed out.
-   *
-   * @param {string} dialogId The ID of the dialog to watch.
-   */
-  var watch = function(dialogId) {
-    timeoutIds[dialogId] = window.setTimeout(function() {
-      intercom.emit(dialogId, 'lost');
-    }, DIALOG_TIMEOUT_MS);
-  };
-
-  /**
-   * Stop watching the given dialog.
-   * @param {string} dialogId The ID of the dialog to watch.
-   */
-  var forget = function(dialogId) {
-    if (timeoutIds[dialogId]) {
-      window.clearTimeout(timeoutIds[dialogId]);
-    }
-  };
 
   this.init();
 };
