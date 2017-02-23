171b3878f4e8ba94452780c94cb9ee566e2a339d
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-02-23 12:54:51 -0700

    Built the new Merge services.

---
+++ src/GAS/merge-template-service.js
@@ -0,0 +1,180 @@
+
+
+
+/**
+ * This service allows easy access to the Mailman template data.
+ *
+ * @type {Object}
+ */
+var MergeTemplateService = {
+  SHEET_NAME: 'mm-config',
+  ID_INDEX: 1,
+  DATA_INDEX: 2,
+
+  //***** public methods *****//
+
+  /**
+   * Gets all MergeTemplates.
+   *
+   * @return {string} A stringified array (<Array<MergeTemplate>).
+   */
+  getAll: function() {
+    try {
+      var sheet = MergeTemplateService.getTemplateSheet();
+      var range = sheet.getDataRange();
+      var rObj = {
+        templates: []
+      };
+
+      for (var i = 0; i < range.getNumRows(); i++) {
+        var row = range.offset(i, 0, 1, range.getNumColumns());
+        try {
+          var config = JSON.parse(row.getCell(1, MergeTemplateService.DATA_INDEX).getDisplayValue());
+          rObj.templates.push(config);
+        }
+        catch (e) {
+          // Potentially delete the template.
+          log('Invalid JSON discovered.');
+        }
+      }
+
+      return rObj;
+    }
+    catch (e) {
+      log(e);
+      throw e;
+    }
+  },
+
+  /**
+   * Gets a MergeTemplate by id. Note that the returned object is just a string.
+   * Use JSON.parse if you want the actual Object.
+   *
+   * @param  {string} id The id of the MergeTemplate to return.
+   * @return {string} A stringified version of the MergeTemplate.
+   */
+  getByID: function(id) {
+    try {
+      var row = MergeTemplateService.getRowByID(id);
+      if (row === null) {
+        return {};
+      }
+
+      return row.getCell(1, MergeTemplateService.DATA_INDEX).getDisplayValue();
+    }
+    catch (e) {
+      log(e);
+      throw e;
+    }
+  },
+
+  /**
+   * Deletes a MergeTemplate by id.
+   *
+   * @param  {string} id The id of the MergeTemplate to delete.
+   */
+  deleteByID: function(id) {
+    try {
+      var sheet = MergeTemplateService.getTemplateSheet();
+      var row = MergeTemplateService.getRowByID(id);
+
+      if (row !== null) {
+        sheet.deleteRow(row.getRowIndex());
+      }
+    }
+    catch (e) {
+      log(e);
+      throw e;
+    }
+  },
+
+  /**
+   * Creates a new MergeTemplate.
+   *
+   * @param  {MergeTemplate} template The stringified version of the MergeTemplate to create.
+   */
+  create: function(template) {
+    try {
+      // We need to verify there is a timestamp column.
+      var dataSheet = getSpreadsheet().getSheetByName(template.sheet);
+      if (dataSheet !== null) {
+        var headers = getHeaderStrings(template);
+        var name = template.timestampColumn
+          .replace('<<', '')
+          .replace('>>', '');
+
+        if (headers.indexOf(name) === -1) {
+          appendColumn(template, name);
+        }
+      }
+
+      var sheet = MergeTemplateService.getTemplateSheet();
+
+      sheet.appendRow([template.id, JSON.stringify(template)]);
+    }
+    catch (e) {
+      log(e);
+      throw e;
+    }
+  },
+
+  /**
+   * Updates an existing MergeTemplate. MergeTemplate.id is used to do the comparison.
+   *
+   * @param  {MergeTemplate} template The new MergeTemplate.
+   */
+  update: function(template) {
+    try {
+      // We need to verify there is a timestamp column.
+      var dataSheet = getSpreadsheet().getSheetByName(template.sheet);
+      if (dataSheet !== null) {
+        var headers = getHeaderStrings(template);
+        var name = template.timestampColumn
+          .replace('<<', '')
+          .replace('>>', '');
+
+        if (headers.indexOf(name) === -1) {
+          appendColumn(template, name);
+        }
+      }
+
+      log('updating: ' + template.id);
+      var row = getRowByID(template.id);
+
+      if (row == null) {
+        throw new Error('Template ' + template.id + ' does not exist.');
+      }
+
+      var cell = row.getCell(1, MergeTemplateService.DATA_INDEX);
+      cell.setValue(JSON.stringify(cell));
+    }
+    catch (e) {
+      log(e);
+      throw e;
+    }
+  },
+
+  //***** private methods *****//
+
+  getRowByID: function(id) {
+    var sheet = MergeTemplateService.getTemplateSheet();
+    var range = sheet.getDataRange();
+
+    for (var i = 0; i < range.getNumRows(); i++) {
+
+      var row = range.offset(i, 0, 1, range.getNumColumns());
+      var idCell = row.getCell(1, MergeTemplateService.ID_INDEX);
+
+      if (idCell.getDisplayValue() === id) {
+        return row;
+      }
+    }
+
+    return null;
+  },
+
+  getTemplateSheet: function() {
+    var ss = getSpreadsheet();
+    return ss.getSheetByName(MergeTemplateService.SHEET_NAME);
+  }
+};
---
+++ src/client/js/services/merge-template-service.js
@@ -0,0 +1,34 @@
+var Provoke = require('../util/provoke.js');
+
+
+
+var MergeTemplateService = function() {
+
+  //***** private methods *****//
+
+
+  //***** public methods *****//
+
+  this.create = function(template) {
+    return Provoke('MergeTemplateService', 'create', template.toConfig());
+  };
+
+  this.getAll = function() {
+    return Provoke('MergeTemplateService', 'getAll');
+  };
+
+  this.getOne = function(id) {
+    return Provoke('MergeTemplateService', 'getByID', id);
+  };
+
+  this.update = function(template) {
+    return Provoke('MergeTemplateService', 'update', template.toConfig());
+  };
+
+  this.delete = function(template) {
+    return Provoke('MergeTemplateService', 'delete', template.getID());
+  }
+};
+
+
+module.exports = MergeTemplateService;
