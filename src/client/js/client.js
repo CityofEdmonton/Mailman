aecf99a2ac9b1fe6292b1795f2e9c7cd0922b3cc
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-02-24 14:43:54 -0700

    Removed logging.

--- src/client/js/client.js
+++ src/client/js/client.js
@@ -6,15 +6,11 @@ var MailMan = require('./mailman.js');
 var Util = require('./util.js');
 var LoadingScreen = require('./loading/loading-screen.js');
 
-
-
 $(document).ready(function() {
   var body = $(document).find('[data-id="body"]');
   var ls = LoadingScreen;
   ls.init(body);
   ls.show();
-
-  console.log('Loading screen');
 
   var mailman = new MailMan(body);
 
