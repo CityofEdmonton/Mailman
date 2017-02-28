0585fe1a96baa19275d9cd15d9f7f040d39c67cc
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-02-28 14:58:18 -0700

    Got the old logger to use the new SettingsService.

--- src/gas/logger.js
+++ src/gas/logger.js
@@ -2,20 +2,16 @@
 // This is the logging spreadsheet I'm using.
 var logSheet;
 var maxRows = 3000;
-var MAILMAN_LOG_NAME = 'mailman_log';
-var SS_KEY = 'MAILMAN_LOG_URL';
 
 function log(text) {
   try {
-    if (logSheet === undefined) {
-      var url = getLogURL();
-
-      if(url === null) {
-        return;
-      }
-
-      logSheet = SpreadsheetApp.openByUrl(url);
+    getLogSheet();
+    Logger.log(logSheet);
+    if (logSheet == null) {
+      return;
     }
+
+
     if (logSheet.getLastRow() > maxRows) {
       var sheet = logSheet.getSheets()[0];
       sheet.getRange(2, 1, logSheet.getLastRow() - 1, logSheet.getLastColumn()).clear();
@@ -25,42 +21,21 @@ function log(text) {
   }
   catch (e) {
     // This tends to be the user not having logging permissions to the Sheet.
+    throw e;
   }
 
 }
 
-function turnOnLogging() {
-  var ss = SpreadsheetApp.create(MAILMAN_LOG_NAME);
+function getLogSheet() {
+  Logger.log('test');
+  if (logSheet == null) {
+    var url = SettingsService.getLogURL();
+    Logger.log(url);
 
-  // The log file needs to be editable by anyone that could use Mailman.
-  // Ideally, this log is temporary.
-  var file = DriveApp.getFileById(ss.getId());
-  file.setSharing(DriveApp.Access.DOMAIN, DriveApp.Permission.EDIT);
+    if(url === null) {
+      return;
+    }
 
-  var url = ss.getUrl();
-
-  var prop = PropertiesService.getDocumentProperties();
-  prop.setProperty(SS_KEY, url);
-
-  return url;
-};
-
-function turnOffLogging() {
-  var prop = PropertiesService.getDocumentProperties();
-  var url = prop.getProperty(SS_KEY);
-  prop.deleteProperty(SS_KEY);
-
-  if (url === null) {
-    return;
+    logSheet = SpreadsheetApp.openByUrl(url);
   }
-
-  var ss = SpreadsheetApp.openByUrl(url);
-  var id = ss.getId();
-  var file = DriveApp.getFileById(id);
-  file.setTrashed(true);
-};
-
-function getLogURL() {
-  var prop = PropertiesService.getDocumentProperties();
-  return prop.getProperty(SS_KEY);
-};
+}
