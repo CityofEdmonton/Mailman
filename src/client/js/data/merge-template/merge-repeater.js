f3f6dca2124e65eaa24c3d5d65012f62df7de13b
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-03-02 13:04:39 -0700

    Updated the fields needed for a repeated merge.

--- src/client/js/data/merge-template/merge-repeater.js
+++ src/client/js/data/merge-template/merge-repeater.js
@@ -1,14 +1,61 @@
 
 
+/**
+ * The MergeRepeater handles storing data related to repeated merges. This doesn't actually create the trigger
+ * or talk to the server. This Object can be easily serialized by calling toConfig or deserialized by passing that
+ * config back into this contructor.
+ *
+ * @constructor
+ * @param {Object} config The configuration Object used to define this MergeRepeater.
+ * @param {Array<string>} config.triggers An Array of trigger IDs that can call this merge.
+ * @param {string} config.owner The user that added the MergeRepeater.
+ * @param {Array<string>} config.events A log of events that pertain to this MergeRepeater.
+ */
 var MergeRepeater = function(config) {
+
+  var MAX_EVENTS = 10;
+
+  var triggers;
+  var owner;
+  var events;
 
   //***** Private Methods *****//
 
   this.init_ = function(config) {
+    events = [];
 
+    this.update(config);
   };
 
   //***** Public Methods *****//
+
+  this.update = function(config) {
+    if (config.triggers != null) {
+      triggers = config.triggers;
+    }
+    if (config.owner != null) {
+      owner = config.owner;
+    }
+    if (config.events != null) {
+      events = config.events;
+    }
+  }
+
+  /**
+   * Adds an event to this MergeRepeater. This can be used to determine what the trigger has been doing.
+   * This ensures only MAX_EVENTS are recorded. Keep in mind, this is a client-side control, so it could easily be
+   * circumvented.
+   *
+   * @param {string} event The event to log.
+   */
+  this.addEvent = function(event) {
+    events.push(event);
+
+    var tooMany = events.length - MAX_EVENTS;
+    if (tooMany > 0) {
+      events.splice(0, tooMany);
+    }
+  };
 
   /**
    * Converts this object into an easily serializeable form.
@@ -16,7 +63,9 @@ var MergeRepeater = function(config) {
    * @returns {Object} An Object containing all the data needed to recreate this Object.
    */
   this.toConfig = function() {
-
+    triggers: triggers,
+    owner: owner,
+    events: events
   };
 
   this.init_(config);
