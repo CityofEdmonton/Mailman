0b0dbb7aebc34a6865f20080ff896fd1d489694b
j-rewerts <rewerts@ualberta.ca>
2017-03-22 12:27:32 -0600

    Added JSDoc comments to list and node.

--- src/client/js/list/node.js
+++ src/client/js/list/node.js
@@ -1,11 +1,17 @@
-
+/**
+ * This module exports the {@link Node} object.
+ *
+ * @author {@link https://github.com/j-rewerts|Jared Rewerts}
+ * @module
+ */
 
 
 /**
  * A node, often used in List implementations.
  *
- * @param {Object} data The data to be assigned to the node.
  * @constructor
+ * @alias Node
+ * @param {Object} data The data to be assigned to the node.
  */
 var Node = function(data) {
   this.data = data;
