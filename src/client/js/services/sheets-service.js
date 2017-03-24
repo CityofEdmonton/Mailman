16d2d2fa66babc5af7742016ba4c8bd98e01376a
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-03-24 16:27:39 -0600

    Added caching to SheetsService.

--- src/client/js/services/sheets-service.js
+++ src/client/js/services/sheets-service.js
@@ -1,5 +1,5 @@
 /**
- * This module exports the SheetsService object.
+ * This module exports the SheetsService object as a singleton.
  *
  * @author {@link https://github.com/j-rewerts|Jared Rewerts}
  * @module
@@ -10,11 +10,28 @@ var Provoke = require('../util/provoke.js');
 
 
 /**
- * Handles operations related to getting Sheet names.
+ * Handles operations related to getting Sheet names. This object is used as a singleton.
+ * It has basic caching abilties.
  *
  * @constructor
  */
 var SheetsService = function() {
+
+  var sheets = null;
+  var REFRESH_DURATION = 30000;
+  var self = this;
+
+  //**** private functions ****//
+
+  this.init_ = function() {
+    window.setInterval(resetCache, REFRESH_DURATION);
+  };
+
+  var resetCache = function() {
+    sheets = null;
+  };
+
+  //**** public functions ****//
 
   /**
    * Gets all Sheet names.
@@ -22,11 +39,16 @@ var SheetsService = function() {
    * @return {Promise} A Promise.
    */
   this.get = function() {
-    return Provoke('SheetsService', 'get');
+    if (sheets === null) {
+      sheets = Provoke('SheetsService', 'get');
+    }
+
+    return sheets;
   };
 
+  self.init_();
 };
 
 
 /** */
-module.exports = SheetsService;
+module.exports = new SheetsService();
