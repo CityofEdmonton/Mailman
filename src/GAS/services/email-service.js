4d39d9c6d6e77c9ca1119a60220b0d2b84b9dc15
j-rewerts <jaredrewerts@gmail.com>
2017-09-25 10:59:37 -0600

    HTML and plain text emails can be sent based on their content.

--- src/GAS/services/email-service.js
+++ src/GAS/services/email-service.js
@@ -120,20 +120,25 @@ var EmailService = {
       return false;
     }
 
-    var htmlEmail = HtmlService.createTemplateFromFile('email-template');
-    htmlEmail.id = Utility.getSpreadsheet().getId();
-    htmlEmail.bodyArray = body.split('\n');
-
     log('Sending email to ' + JSON.stringify({
       to: to,
       cc: cc,
       bcc: bcc
     }));
-    GmailApp.sendEmail(to, subject, body, {
-      htmlBody: htmlEmail.evaluate().getContent(),
-      cc: cc,
-      bcc: bcc
-    });
+
+    if (/<html>/.test(body)) {
+      GmailApp.sendEmail(to, subject, body, {
+        htmlBody: body,
+        cc: cc,
+        bcc: bcc
+      });
+    }
+    else {
+      GmailApp.sendEmail(to, subject, body, {
+        cc: cc,
+        bcc: bcc
+      });
+    }
 
     return true;
   },
