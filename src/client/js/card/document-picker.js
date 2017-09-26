23426c3b3e48fbd2320d37775336c650053d73e7
j-rewerts <jaredrewerts@gmail.com>
2017-09-26 08:53:37 -0600

    Cleaned up references to inputs.

--- src/client/js/card/document-picker.js
+++ src/client/js/card/document-picker.js
@@ -1,5 +1,5 @@
 /**
- * This module exports an InputCard object.
+ * This module exports an DocumentPickerCard object.
  *
  * @author {@link https://github.com/j-rewerts|Jared Rewerts}
  * @module
@@ -12,20 +12,14 @@ var Util = require('../util/util.js');
 
 
 /**
- * InputCards use a 1 line input field. They differ from TextareaCard in this way.
+ * This card allows a user to pick a file from their Drive.
  *
  * @constructor
  * @extends module:client/js/card/card-titled~TitledCard
  * @param {jquery} appendTo The object to append this Card to.
- * @param {Object} options The configuration options for this InputCard.
- * @param {String} options.label The label the input should have when it has no text.
- * @param {Object} options.autocomplete The autocomplete configuration object. Please see setAutocomplete for a more
- *  detailed listing of this Object.
- * @param {Object} options.error The Object containing the information needed for this input to support input validation.
- * @param {string} options.error.hint The text displayed when text is invalid.
- * @param {string} options.error.pattern The regex pattern to match against.
+ * @param {Object} options The configuration options for this card.
  */
-var InputCard = function(appendTo, options) {
+var DocumentPickerCard = function(appendTo, options) {
   TitledCard.call(this, appendTo, options);
 
   // Private variables
@@ -33,7 +27,6 @@ var InputCard = function(appendTo, options) {
   var innerBase = $(html);
 
   var mdlObject = innerBase.find('[data-id="mdl-object"]');
-  var input = innerBase.find('input');
 
   //***** Private Methods *****//
 
@@ -45,55 +38,12 @@ var InputCard = function(appendTo, options) {
   //***** Public Methods *****//
 
   /**
-   * Gets the value in the input.
+   * Gets the document id selected by the user.
    *
-   * @return {string} The value in the input.
+   * @return {string|null} The id of the document.
    */
   this.getValue = function() {
-    return input.val();
-  };
-
-  /**
-   * Sets the value of the input.
-   *
-   * @param {string} value The value to set in the input.
-   */
-  this.setValue = function(value) {
-    mdlObject[0].MaterialTextfield.change(value);
-  };
-
-  /**
-   * Sets the label shown in the input when nothing has been typed.
-   *
-   * @param {string} label The value to set as the label.
-   */
-  this.setLabel = function(label) {
-    innerBase.find('label').text(label);
-  };
-
-  /**
-   * Gets the HTMLInput object as a jquery object.
-   *
-   * @return {jquery} The input object.
-   */
-  this.getTextElement = function() {
-    return input;
-  };
-
-  /**
-   * This disables the input element.
-   *
-   */
-  this.disableInput = function() {
-    mdlObject[0].MaterialTextfield.disable();
-  };
-
-  /**
-   * This enables the input element.
-   *
-   */
-  this.enableInput = function() {
-    mdlObject[0].MaterialTextfield.enable();
+    return null;
   };
 
   this.init_(appendTo, options);
@@ -101,9 +51,9 @@ var InputCard = function(appendTo, options) {
 
 
 /** */
-InputCard.prototype.constructor = InputCard;
-InputCard.prototype = Object.create(TitledCard.prototype);
+DocumentPickerCard.prototype.constructor = DocumentPickerCard;
+DocumentPickerCard.prototype = Object.create(TitledCard.prototype);
 
 
 /** */
-module.exports = InputCard;
+module.exports = DocumentPickerCard;
