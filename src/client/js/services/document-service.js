fb2860ec34b7c96df32a0068a1c3caf74d2a5b8a
Dan Chenier <daniel.chenier@edmonton.ca>
2017-11-28 09:37:39 -0700

    Removed LocalStorage references
    Some users block the usage of localstorage - so have removed it altogether since its not needed

--- src/client/js/services/document-service.js
+++ src/client/js/services/document-service.js
@@ -1,65 +1,65 @@
-/**
- * This module exports the DocumentService object.
- *
- * @author {@link https://github.com/j-rewerts|Jared Rewerts}
- * @module
- */
+// /**
+//  * This module exports the DocumentService object.
+//  *
+//  * @author {@link https://github.com/j-rewerts|Jared Rewerts}
+//  * @module
+//  */
 
 
-var Provoke = require('../util/provoke.js');
-var EventEmitter = require('../event-emitter/local-storage-emitter.js');
+// var Provoke = require('../util/provoke.js');
+// //var EventEmitter = require('../event-emitter/local-storage-emitter.js');
 
 
-/**
- * This service handles getting information about a Google Doc.
- *
- * @constructor
- */
-var DocumentService = function() {
+// /**
+//  * This service handles getting information about a Google Doc.
+//  *
+//  * @constructor
+//  */
+// var DocumentService = function() {
 
-  var key = 'DOC_PICKER_RESPONSE';
-  var emitter = new EventEmitter();
+//   var key = 'DOC_PICKER_RESPONSE';
+//   var emitter = new EventEmitter();
 
-  //***** private methods *****//
+//   //***** private methods *****//
 
-  this.init_ = function() {
+//   this.init_ = function() {
 
-  };
+//   };
 
-  //***** public methods *****//
+//   //***** public methods *****//
 
-  /**
-   * Gets an ID for a Google Doc.
-   *
-   * @return {Promise} A Promise that resolves when the user has selected or cancelled the picker.
-   */
-  this.getDocument = function() {
-    return Provoke('PickerService', 'openDocument', key).then(() => {
-      return emitter.once(key);
-    });
-  };
+//   /**
+//    * Gets an ID for a Google Doc.
+//    *
+//    * @return {Promise} A Promise that resolves when the user has selected or cancelled the picker.
+//    */
+//   this.getDocument = function() {
+//     return Provoke('PickerService', 'openDocument', key).then(() => {
+//       return emitter.once(key);
+//     });
+//   };
 
-  /**
-   * Gets the thumbnail URL for a Google Doc.
-   * @param {string} id The id of the document.
-   * @return {string} The URL of the docs thumbnail.
-   */
-  this.getThumbnail = function(id) {
-    return Provoke('DocumentService', 'getThumbnail', id);
-  };
+//   /**
+//    * Gets the thumbnail URL for a Google Doc.
+//    * @param {string} id The id of the document.
+//    * @return {string} The URL of the docs thumbnail.
+//    */
+//   this.getThumbnail = function(id) {
+//     return Provoke('DocumentService', 'getThumbnail', id);
+//   };
 
-  /**
-   * Gets the thumbnail URL for a Google Doc.
-   * @param {string} id The id of the document.
-   * @return {Object} The Docs metadata.
-   */
-  this.getMetadata = function(id) {
-    return Provoke('DocumentService', 'getMetadata', id);
-  };
+//   /**
+//    * Gets the thumbnail URL for a Google Doc.
+//    * @param {string} id The id of the document.
+//    * @return {Object} The Docs metadata.
+//    */
+//   this.getMetadata = function(id) {
+//     return Provoke('DocumentService', 'getMetadata', id);
+//   };
 
-  this.init_();
-};
+//   this.init_();
+// };
 
 
-/** */
-module.exports = new DocumentService();
+// /** */
+// module.exports = new DocumentService();
