907947cbe2ff0c51c7a88a24416be524ad15e457
Jared Rewerts <jaredrewerts@gmail.com>
2018-09-17 12:12:11 -0600

    Cleaned/formatted responsive nav menu.

--- src/Mailman.Server/ClientApp/src/components/navigation/ResponsiveNavMenu.js
+++ src/Mailman.Server/ClientApp/src/components/navigation/ResponsiveNavMenu.js
@@ -1,28 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Hidden from '@material-ui/core/Hidden';
-import Divider from '@material-ui/core/Divider';
-import MenuIcon from '@material-ui/icons/Menu';
-
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import StarIcon from '@material-ui/icons/Star';
-import SendIcon from '@material-ui/icons/Send';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
 // State Management
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { actionCreators } from '../../store/NavDrawer';
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { actionCreators } from "../../store/NavDrawer";
 
 const drawerWidth = 240;
 
@@ -31,41 +19,41 @@ const styles = theme => ({
     flexGrow: 1,
     height: 440,
     zIndex: 1,
-    overflow: 'hidden',
-    position: 'relative',
-    display: 'flex',
-    width: '100%',
+    overflow: "hidden",
+    position: "relative",
+    display: "flex",
+    width: "100%"
   },
   appBar: {
-    position: 'absolute',
+    position: "absolute",
     marginLeft: drawerWidth,
-    [theme.breakpoints.up('md')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-    },
+    [theme.breakpoints.up("md")]: {
+      width: `calc(100% - ${drawerWidth}px)`
+    }
   },
   navIconHide: {
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
   },
   toolbar: theme.mixins.toolbar,
   drawerPaper: {
     width: drawerWidth,
-    [theme.breakpoints.up('md')]: {
-      position: 'relative',
-    },
+    [theme.breakpoints.up("md")]: {
+      position: "relative"
+    }
   },
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3,
-  },
+    padding: theme.spacing.unit * 3
+  }
 });
 
 class ResponsiveNavMenu extends React.Component {
   render() {
-    const { classes, theme } = this.props;
-    console.log(`State: ${this.props}`)
+    const { classes } = this.props;
+    console.log(`State: ${this.props}`);
 
     return (
       <div className={classes.root}>
@@ -99,10 +87,12 @@ ResponsiveNavMenu.propTypes = {
 const mapStateToProps = state => {
   return {
     drawerOpen: state.navDrawer.drawerOpened
-  }
-}
+  };
+};
 
-const exportWithStyles = withStyles(styles, { withTheme: true })(ResponsiveNavMenu);
+const exportWithStyles = withStyles(styles, { withTheme: true })(
+  ResponsiveNavMenu
+);
 
 export default connect(
   mapStateToProps,
