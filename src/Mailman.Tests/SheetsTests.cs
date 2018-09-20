cdf120cdab6f50cc17ead72abaf0bd3b7daed110
Dan Chenier <daniel.chenier@edmonton.ca>
2018-09-19 21:39:38 -0600

    fix tests

--- src/Mailman.Tests/SheetsTests.cs
+++ src/Mailman.Tests/SheetsTests.cs
@@ -23,33 +23,75 @@ namespace Mailman.Tests
     {
         public SheetsTests()
         {
-            // Our build server downloads this secure file to this location
-            string credFilePath = Environment.GetEnvironmentVariable("DOWNLOADSECUREFILE_SECUREFILEPATH");
-            dynamic serviceAccount = JObject.Parse(File.ReadAllText(credFilePath));
             TEST_SHEET_ID = Environment.GetEnvironmentVariable("GOOGLE_TEST_SHEET");
-            
-            var credential = new ServiceAccountCredential(new ServiceAccountCredential.Initializer(serviceAccount.client_email)
+            if (string.IsNullOrWhiteSpace(TEST_SHEET_ID))
             {
-                Scopes = GetCredentialScopes()
-            }.FromPrivateKey(serviceAccount.private_key));
-            
+                // this is a test sheet with public access in the edmonton.ca domain
+                TEST_SHEET_ID = "1GnoG6twy6OC9jQw7-KeZBR02znTW8VkR7Yp2Wf2JlrY";
+            }
+
+            var services = new ServiceCollection()
+                .AddScoped(x => SetupMockSheetsServiceFactory())
+                .AddScoped<SheetsController>()
+                .BuildServiceProvider();
+
+            _sheetsController = services.GetRequiredService<SheetsController>();
+        }
+
+        private static ISheetsServiceFactory SetupMockSheetsServiceFactory()
+        {
             var serviceInitializer = new Google.Apis.Services.BaseClientService.Initializer()
             {
-                HttpClientInitializer = credential
+                HttpClientInitializer = GetGoogleCredential()
             };
             var sheetsService = Task.FromResult(new SheetsService(serviceInitializer));
 
             var sheetsServiceFactory = new Mock<ISheetsServiceFactory>();
             sheetsServiceFactory.Setup(x => x.GetSheetsServiceAsync()).Returns(sheetsService);
 
-            var services = new ServiceCollection()
-                .AddScoped(x => sheetsServiceFactory.Object)
-                .AddScoped<SheetsController>()
-                .BuildServiceProvider();
-
-            _sheetsController = services.GetRequiredService<SheetsController>();
+            return sheetsServiceFactory.Object;
         }
-        
+
+
+        private static Google.Apis.Http.IConfigurableHttpClientInitializer _googleCredential;
+        private static Google.Apis.Http.IConfigurableHttpClientInitializer GetGoogleCredential()
+        {
+            if (_googleCredential == null)
+            {
+                // Our build server downloads this secure file to this location
+                string credFilePath = Environment.GetEnvironmentVariable("DOWNLOADSECUREFILE_SECUREFILEPATH");
+
+                // you can also drop a .json service credential file into the tests directory (it is .gitignored)
+                if (string.IsNullOrWhiteSpace(credFilePath))
+                    credFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Mailman-service-account.json");
+
+                if (string.IsNullOrWhiteSpace(credFilePath))
+                    throw new InvalidOperationException("A service credential must be specified by placing a file named 'Mailman-service-account.json' in the tests directory");
+
+                if (!File.Exists(credFilePath))
+                    throw new InvalidOperationException("A valid service credential must be specified (file specified but doesn't exist on disk): " + credFilePath);
+
+                try
+                {
+                    dynamic serviceAccount = JObject.Parse(File.ReadAllText(credFilePath));
+
+                    string clientEmail = serviceAccount.client_email;
+                    string privateKey = serviceAccount.private_key;
+                    _googleCredential = new ServiceAccountCredential(new ServiceAccountCredential.Initializer(clientEmail)
+                    {
+                        Scopes = GetCredentialScopes()
+                    }.FromPrivateKey(privateKey));
+                }
+                catch (Exception err)
+                {
+                    throw new InvalidOperationException("A valid service credential must be specified (file specified but couldn't read json file correctly): " + err.Message, err);
+                }
+            }
+
+            return _googleCredential;
+        }
+
+
         private static string[] GetCredentialScopes()
         {
             return new[] {
