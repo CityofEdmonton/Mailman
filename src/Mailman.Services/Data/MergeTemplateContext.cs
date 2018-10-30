b9624df3cdc8a693bb4b9b402be4ba9eb3effa99
Zhipeng-Chang <zchang0302@gmail.com>
2018-10-30 16:56:52 -0600

    added some code based on the error message
    error message Unable to create an object of type 'MergeTemplateContext'. Add an implementation of 'IDesignTimeDbContextFactory<MergeTemplateContext>' to the project, or see https://go.microsoft.com/fwlink/?linkid=851728 for additional patterns supported at design time.

--- src/Mailman.Services/Data/MergeTemplateContext.cs
+++ src/Mailman.Services/Data/MergeTemplateContext.cs
@@ -1,4 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Mailman.Services.Data
 {
@@ -7,10 +11,21 @@ namespace Mailman.Services.Data
         public MergeTemplateContext(DbContextOptions<MergeTemplateContext> options) : base(options) { }
 
         public DbSet<MergeTemplate> MergeTemplates {get;set;} 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Data Source=mergetemplate.db");
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("Data Source=mergetemplate.db");
+        //}
         
     }
+
+    public class MergeTemplateContextFactory : IDesignTimeDbContextFactory<MergeTemplateContext>
+    {
+    public MergeTemplateContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<MergeTemplateContext>();
+        builder.UseSqlite("Data Source=mergetemplate.db");
+
+        return new MergeTemplateContext(builder.Options);
+    }
+}
 }
