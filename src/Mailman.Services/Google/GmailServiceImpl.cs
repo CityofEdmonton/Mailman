aff5d82a07cfcc401ea47e6a640c1c78085993df
Zhipeng-Chang <zchang0302@gmail.com>
2018-11-07 16:50:37 -0700

    add gmail service, need to get the token

--- src/Mailman.Services/Google/GmailServiceImpl.cs
+++ src/Mailman.Services/Google/GmailServiceImpl.cs
@@ -4,6 +4,10 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Gmail.v1;
+using System.Net.Mail;
+using System.IO;
 
 namespace Mailman.Services.Google
 {
@@ -30,9 +34,40 @@ namespace Mailman.Services.Google
         {
             using (var gmailservice = await _googleSheetsServiceAccessor.GetGmailServiceAsync())
             {
-                throw new NotImplementedException();
+                var msg = new AE.Net.Mail.MailMessage {
+                    Subject = subject,
+                    Body = body,
+                };
+                msg.To.Add(new MailAddress(to));
+                var msgStr = new StringWriter();
+                msg.Save(msgStr);
+
+                // Context is a separate bit of code that provides OAuth context;
+                // your construction of GmailService will be different from mine.
+
+                var message = new Message {Raw = Base64UrlEncode(msgStr.ToString())};
+                try
+                {   var service = new GmailService(); // should be var service = new GmailService(Google authorization token);
+                    var result =  service.Users.Messages.Send(message, "me").Execute();
+                    _logger.Information("Just sent the email");
+
+                }
+                catch (Exception e)
+                {
+                    _logger.Error("An error occurred: " + e.Message);
+                }
             }
 
+        }
+
+        private static string Base64UrlEncode(string input) 
+        {
+            var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+            // Special "url-safe" base64 encode.
+            return Convert.ToBase64String(inputBytes)
+                .Replace('+', '-')
+                .Replace('/', '_')
+                .Replace("=", "");
         }
     }
 }
