70fe6fbd8e7924eefb04b9263fdf3cae87bca5c5
Zhipeng-Chang <zchang0302@gmail.com>
2018-11-08 15:54:31 -0700

    remove not used function

--- src/Mailman.Services/Google/GmailServiceImpl.cs
+++ src/Mailman.Services/Google/GmailServiceImpl.cs
@@ -107,28 +107,6 @@ namespace Mailman.Services.Google
                 .Replace('/', '_')
                 .Replace("=", "");
         } 
-
-        private static string HtmlToPlainText(string html)
-        {
-            const string tagWhiteSpace = @"(>|$)(\W|\n|\r)+<";//matches one or more (white space or line breaks) between '>' and '<'
-            const string stripFormatting = @"<[^>]*(>|$)";//match any character between '<' and '>', even when end tag is missing
-            const string lineBreak = @"<(br|BR)\s{0,1}\/{0,1}>";//matches: <br>,<br/>,<br />,<BR>,<BR/>,<BR />
-            var lineBreakRegex = new Regex(lineBreak, RegexOptions.Multiline);
-            var stripFormattingRegex = new Regex(stripFormatting, RegexOptions.Multiline);
-            var tagWhiteSpaceRegex = new Regex(tagWhiteSpace, RegexOptions.Multiline);
-
-            var text = html;
-            //Decode html specific characters
-            text = System.Net.WebUtility.HtmlDecode(text); 
-            //Remove tag whitespace/line breaks
-            text = tagWhiteSpaceRegex.Replace(text, "><");
-            //Replace <br /> with line breaks
-            text = lineBreakRegex.Replace(text, Environment.NewLine);
-            //Strip formatting
-            text = stripFormattingRegex.Replace(text, string.Empty);
-
-            return text;
-        }
         
     }
 }
